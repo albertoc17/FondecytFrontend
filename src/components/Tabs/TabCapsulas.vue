ec3a0c537408e5732e4b2d4f0db481c31338f9b7
Alberto Cartagena Cifuentes <albertoc1717@gmail.com>
2022-01-23 23:12:00 -0300

    se agregan las capsulas finales

--- src/components/Tabs/TabCapsulas.vue
+++ src/components/Tabs/TabCapsulas.vue
@@ -1,17 +1,14 @@
 <template>
-    <div>
+    <div class="capsulaContainer">
       <div
           v-for="(capsula, index) in capsulas"
           :key="index"
            class= "capsulaWrapper"
       >
         <iframe class="img-wrap" :src="capsula" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      
-      <div class="cont-txt">
-        <h5 class="subtit text">
-          La mecánica cuántica es la rama de la física que estudia la naturaleza a escalas espaciales pequeñas.
+        <h5 class="text">
+          Capsula {{ index + 1 }}
         </h5>
-      </div> 
     </div>
   </div>
 </template>
@@ -42,18 +39,21 @@ export default {
 
 <style scoped>
 .capsulaContainer {
-  display: flex;
-  flex-direction: row;
+  max-height: 85vh; 
+  overflow-y: scroll;
 }
 .capsulaWrapper {
   display: flex;
   flex-direction: row;
   padding: 2% 0;
+  
 }
 .text {
+  width: 150px;
   color: #374957;
   line-height: 1.5;
   font-family: "Lato-Bold", Helvetica Neue, -apple-system, "system-ui", BlinkMacSystemFont, sans-serif;
-  margin: 2%;
+  margin-left: 5%;
+
 }
 </style>
