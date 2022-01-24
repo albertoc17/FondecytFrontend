45ecb4e55787863dedddc561a1e635ce34a7d461
Alberto Cartagena Cifuentes <albertoc1717@gmail.com>
2022-01-23 23:50:35 -0300

    clean code 2

--- src/components/Sidenav.vue
+++ src/components/Sidenav.vue
@@ -7,7 +7,6 @@
       </a>
     </figure>
     <h2 class="subtit">Tipos de Análisis</h2>
-
     <section class="cont-accordion">
       <div v-for="(a, index) in analisis" :key="index" class="row-accordion">
         <div
@@ -25,65 +24,6 @@
         </div>
         <SubSidenav :analysisTypes="a.analysisTypes" />
       </div>
-      <!-- <div class="row-accordion">
-        <div class="enc-accordion">
-          <div class="content">
-            <span class="ic-txt"></span>
-            <p>Formal</p>
-          </div>
-          <span class="btn-accordion"></span>
-        </div>
-        <ul class="panel">
-          <li>
-            <a href=""> Oraciones <span class="arrow"></span></a>
-          </li>
-          <li>
-            <a href=""> Párrafos <span class="arrow"></span></a>
-          </li>
-        </ul>
-      </div>
-      <div class="row-accordion">
-        <div class="enc-accordion">
-          <div class="content">
-            <span class="ic-txt"></span>
-            <p>Estilo</p>
-          </div>
-          <span class="btn-accordion"></span>
-        </div>
-        <ul class="panel">
-          <li>
-            <a href="">
-              Primera y segunda persona <span class="arrow"></span>
-            </a>
-          </li>
-          <li>
-            <a href=""> Voz pasiva <span class="arrow"></span></a>
-          </li>
-          <li>
-            <a href=""> Conectores <span class="arrow"></span></a>
-          </li>
-        </ul>
-      </div>
-      <div class="row-accordion">
-        <div class="enc-accordion">
-          <div class="content">
-            <span class="ic-txt"></span>
-            <p>Discursivo</p>
-          </div>
-          <span class="btn-accordion"></span>
-        </div>
-        <ul class="panel">
-          <li>
-            <a href=""> Complejidad <span class="arrow"></span></a>
-          </li>
-          <li>
-            <a href=""> Lecturabilidad <span class="arrow"></span></a>
-          </li>
-          <li>
-            <a href=""> Propósito <span class="arrow"></span></a>
-          </li>
-        </ul>
-      </div> -->
     </section>
   </div>
 </template>
